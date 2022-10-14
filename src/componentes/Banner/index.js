ad352eb48b8a8ef235f33d87aadb9680889702b8
WaddFranklin <waddinsohn@gmail.com>
2022-10-14 16:51:27 -0300

    finalizando o curso de react com js da plataforma alura

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,16 @@
+import { Fragment } from 'react';
+import './Banner.css';
+
+function Banner() {
+    // JSX
+    return (
+        <>
+            <header className="banner">
+                <img src="./imagens/banner.png" alt="O banner da página principal do Organo"/>
+            </header>
+            <h1>Teste</h1>
+        </>
+    );
+}
+
+export default Banner;

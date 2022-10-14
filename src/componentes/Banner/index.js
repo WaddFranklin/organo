ff513810d7cbe620a86c3ae39811842155607337
WaddFranklin <waddinsohn@gmail.com>
2022-10-14 16:59:50 -0300

    retirnado a tag de teste do banner

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -1,15 +1,11 @@
-import { Fragment } from 'react';
 import './Banner.css';
 
 function Banner() {
     // JSX
     return (
-        <>
-            <header className="banner">
-                <img src="./imagens/banner.png" alt="O banner da página principal do Organo"/>
-            </header>
-            <h1>Teste</h1>
-        </>
+        <header className="banner">
+            <img src="./imagens/banner.png" alt="O banner da página principal do Organo"/>
+        </header>
     );
 }
 
